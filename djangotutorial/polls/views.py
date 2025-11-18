6300a63def291f0f669c144c33c72f1aede343c2
bayramov733 <bayramovv376@gmail.com>
2025-11-19 00:04:47 +0400

    yeniledim
    .

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,39 +1,40 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.db.models import F
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 from .models import Question, Choice
 
-
+# Index səhifəsi – son 5 sual
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
+# Detail səhifəsi – bir sualın detayı
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
+        """Only show questions published so far."""
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
+# Results səhifəsi – səs nəticələri
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
 
+# Vote funksiyası – birdən çox seçim
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    selected_choices = request.POST.getlist('choices')  # <-- birdən çox seçim
+    selected_choices = request.POST.getlist('choices')
 
     if not selected_choices:
         return render(request, 'polls/detail.html', {
@@ -47,21 +48,3 @@ def vote(request, question_id):
         selected_choice.save()
 
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-    
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
